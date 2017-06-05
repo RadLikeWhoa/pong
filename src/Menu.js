24abc3035c41de52e63f586d9ad9630301d37fc0
Sacha Schmid <schmid.sacha@gmail.com>
2017-06-05 14:18:55 +0200

    Add rules screen

--- src/Menu.js
+++ src/Menu.js
@@ -5,13 +5,14 @@ import './Menu.css'
 class Menu extends React.Component {
   render() {
     return (
-      <main className="menu">
+      <main className="menu full-screen">
         <h1 className="app-title">Pong?!</h1>
         <p>Prepare your pokerface...</p>
         <ul className="unstyled-list selection-list">
           <li><Link to="/game/short" className="button">Short game</Link></li>
           <li><Link to="/game/medium" className="button">Medium game</Link></li>
           <li><Link to="/game/long" className="button">Long game</Link></li>
+          <li><Link to="/rules" className="button">Rules</Link></li>
         </ul>
       </main>
     )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Menu from './Menu'
 import Game from './Game'
+import Rules from './Rules'
 import './index.css'
 
 render(
@@ -10,6 +11,7 @@ render(
     <div>
       <Route exact path="/" component={Menu} />
       <Route path="/game/:difficulty" component={Game} />
+      <Route path="/rules" component={Rules} />
     </div>
   </Router>,
   document.getElementById('game')
